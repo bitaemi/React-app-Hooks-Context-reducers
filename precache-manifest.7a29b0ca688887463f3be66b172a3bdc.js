a87f38bcf03a1f419b655365b934df32c70dacc4
Andreia Bita <andreia.bita@iquestgroup.com>
2020-07-09 17:25:00 +0300

    Updates

---
+++ precache-manifest.7a29b0ca688887463f3be66b172a3bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36189ed13e36245719aa",
+    "url": "/github.io/React-app-Hooks-Context-reducers/static/js/runtime~main.eaee643c.js"
+  },
+  {
+    "revision": "ffaa482d2316401ca2e0",
+    "url": "/github.io/React-app-Hooks-Context-reducers/static/js/main.aac2a6f9.chunk.js"
+  },
+  {
+    "revision": "0a7da73fe27fe52c0ef5",
+    "url": "/github.io/React-app-Hooks-Context-reducers/static/js/2.82744add.chunk.js"
+  },
+  {
+    "revision": "ffaa482d2316401ca2e0",
+    "url": "/github.io/React-app-Hooks-Context-reducers/static/css/main.90031b04.chunk.css"
+  },
+  {
+    "revision": "91fdef8f2d371f896d95a6a4eadf93dc",
+    "url": "/github.io/React-app-Hooks-Context-reducers/index.html"
+  }
+];
